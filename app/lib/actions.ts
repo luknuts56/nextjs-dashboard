50dc8cad5797ba47a6e7062637713c4aacb0cb29
Luke Knutson <luknuts56@gmail.com>
2026-03-20 00:05:25 -0500

    Next.js App Router Fundamentals course Complete!

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -30,6 +32,25 @@ export type State = {
   };
   message: string;
 };
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid email or password.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error; // Re-throw unexpected errors
+  }
+}
 
 // Action function to create a new invoice. It receives the form data, validates it, and then inserts it into the database.
 // After that, it revalidates the invoices page and redirects the user back to the invoices list.
